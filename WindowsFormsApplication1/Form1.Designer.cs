3e302b98b0ffd6a9fe86e374122f18b3359ed139
Brabec <lu_brabec@hotmail.com>
2016-09-19 22:28:59 -0300

    C# Keystroke Project

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(92, 155);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(100, 20);
+            this.input.TabIndex = 0;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(127, 112);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(35, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "label1";
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // btn
+            // 
+            this.btn.Enabled = false;
+            this.btn.Location = new System.Drawing.Point(104, 218);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "calculate";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+
+        public string getString()
+        {
+            string word = this.input.Text;
+            return word;
+        }
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {   
+
+            if (input.Text.Equals(label.Text))
+            {
+                btn.Enabled = true;
+                // se for igual a função db será chamada retornando uma palavra aleatoria
+                // do dicionario
+                this.label.Text = db();
+                this.input.Text = "";
+            }
+
+        }
+
+        public string db()
+        {
+            string[] dicionario = {"quadrado","bola" };
+            Random r = new Random();
+            string v = dicionario[r.Next(0,dicionario.Length)];
+            return v;
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
